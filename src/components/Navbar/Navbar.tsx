aedbaa352c25077e592ee8354608bc8c7d81501c
nayaunglin910 <nayaunglin910@gmail.com>
2024-10-31 22:09:20 +0630

    Add Navbar Logo, Add Button Style

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,8 +1,8 @@
 import { FC, useState } from "react";
 import styles from "./Navbar.module.css";
+import common from "../../styles/common.module.css"
 import { FaBars } from "react-icons/fa6";
 import { IoMdClose } from "react-icons/io";
-import { isWhiteSpaceLike } from "typescript";
 
 export interface NavbarPropsInterface {}
 
@@ -18,39 +18,37 @@ const Navbar: FC<NavbarPropsInterface> = () => {
     : `${styles.nav_menu}`;
 
   return (
-    <nav className={styles.nav}>
-      <a href="#" className={styles.brand}>
-        HOME
-      </a>
-      <ul className={nav}>
-        <li className={styles.nav_item}>
-          <a href="#" className="nav-link">
-            Home
-          </a>
-        </li>
-        <li className={styles.nav_item}>
-          <a href="#" className="nav-link">
-            Sevices
-          </a>
-        </li>
-        <li className={styles.nav_item}>
-          <a href="#" className="nav-link">
-            About
-          </a>
-        </li>
-      </ul>
-      <div className={styles.nav_toggler}>
-        {active ? (
-          <IoMdClose
-            onClick={navToggle}
-            className={styles.nav_icon}
-          />
-        ) : (
-          <FaBars
-            onClick={navToggle}
-            className={styles.nav_icon}
-          />
-        )}
+    <nav className={styles.main_nav}>
+      <div className={styles.brand}>
+        <a href="#">
+          <img src="./golden_link_icon.jpg" className={styles.nav_img} alt="" />
+        </a>
+      </div>
+      <div className={styles.nav}>
+        <ul className={nav}>
+          <li className={styles.nav_item}>
+            <a href="#" className={`${styles.nav_link} ${common.button_gold}`}>
+              Home
+            </a>
+          </li>
+          <li className={styles.nav_item}>
+            <a href="#" className={styles.nav_link}>
+              Sevices
+            </a>
+          </li>
+          <li className={styles.nav_item}>
+            <a href="#" className={styles.nav_link}>
+              About
+            </a>
+          </li>
+        </ul>
+        <div className={styles.nav_toggler}>
+          {active ? (
+            <IoMdClose onClick={navToggle} className={styles.nav_icon} />
+          ) : (
+            <FaBars onClick={navToggle} className={styles.nav_icon} />
+          )}
+        </div>
       </div>
     </nav>
   );
